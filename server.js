4827189f43109c99c4e4c4093261c2f97161a5e5
André Martins <andrebertonhadev@gmail.com>
2019-07-15 14:57:01 -0300

    fcc file metadata microservice project

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+// get uploadt file properties
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size 
+  })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
